0b220b416b2ec1d2e4d49a2d919c80742ae19531
sudeshkunwar@outlook.com <sudeshkunwar@outlook.com>
2020-12-08 22:10:34 +0545

    final app

--- app/src/main/java/com/example/myquizapp/QuizActivity.java
+++ app/src/main/java/com/example/myquizapp/QuizActivity.java
@@ -6,10 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class QuizActivity extends AppCompatActivity {
 
-    Button submitButton;
+    private  Button submitButton;
+    private int score;
+    private RadioGroup numOneRadioGroup;
+    private RadioGroup numTwoRadioGroup;
+    private RadioGroup numThreeRadioGroup;
+
+    private EditText editText;
+    private CheckBox imageCheckbox;
+    private CheckBox movieCheckbox;
+    private CheckBox audioCheckbox;
+    private CheckBox allCheckbox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +30,46 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         submitButton = findViewById(R.id.submit_button);
+
+        numOneRadioGroup = findViewById(R.id.rd_One);
+        numTwoRadioGroup = findViewById(R.id.rd_Two);
+        numThreeRadioGroup = findViewById(R.id.rd_Three);
+
+        editText = findViewById(R.id.OS_Text);
+        imageCheckbox = findViewById(R.id.Image_Checkbox);
+        movieCheckbox = findViewById(R.id.Movie_Checkbox);
+        audioCheckbox = findViewById(R.id.Audio_Checkbox);
+        allCheckbox = findViewById(R.id.All_Checkbox);
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                int score = 0;
+
+                if (numOneRadioGroup.getCheckedRadioButtonId() == R.id.Eighteen_Eighty_Btn){
+                    score += 1;
+                }
+
+                if (numTwoRadioGroup.getCheckedRadioButtonId() == R.id.Field_Btn){
+                    score += 1;
+                }
+
+                String question3Answer = editText.getText().toString();
+                if(question3Answer.equalsIgnoreCase("Operating System")){
+                    score += 1;
+                }
+
+                if (numThreeRadioGroup.getCheckedRadioButtonId() == R.id.Nineteen_zero){
+                    score += 1;
+                }
+
+                if(!imageCheckbox.isChecked() && movieCheckbox.isChecked() && !audioCheckbox.isChecked() && !allCheckbox.isChecked()){
+                    score += 1;
+                }
+
                 Intent intent = new Intent(QuizActivity.this, ScoreActivity.class);
-                intent.putExtra("SCORE", 5);
+                intent.putExtra("SCORE", score);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/myquizapp/ScoreActivity.java
+++ app/src/main/java/com/example/myquizapp/ScoreActivity.java
@@ -5,19 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ScoreActivity extends AppCompatActivity {
 
+    public static String TAG = "ScoreActivity";
+
     private TextView scoreView;
+    private Button backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
-        scoreView = findViewById(R.id.textView2);
+        scoreView = findViewById(R.id.textView);
+        backButton = findViewById(R.id.Back_Btn);
+
         Intent intent = getIntent();
         int score = intent.getIntExtra("SCORE", 0);
-
         scoreView.setText(""+score);
+
+        Log.d(TAG, "score: "+ score);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScoreActivity.this, QuizActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
